37f609a26e38a4b2b109a1fb1d8b8b2fc65aef32
KingLoui <matzehollerbach@web.de>
2016-09-28 12:53:36 +0200

    changed setinngs loading
    only load settings if user is accessing admin prefix

--- src/Controller/Component/BaseKitComponent.php
+++ src/Controller/Component/BaseKitComponent.php
@@ -22,55 +22,59 @@ class BaseKitComponent extends Component
     public function beforeRender(Event $event) {      
     	$controller = $event->subject(); 
 
-
-        // load helpers
-        $controller->viewBuilder()->helpers([
-            'Url', 
-            'Gourmet/KnpMenu.Menu',
-            'Html' => ['className' => 'BootstrapUI.Html'],
-            'Form' => ['className' => 'BootstrapUI.Form'],
-            'Flash' => ['className' => 'BootstrapUI.Flash'],
-            'Paginator' => [
-                'className' => 'BootstrapUI.Paginator',
-                'labels' => [
-                    'prev' => '<i class="fa fa-chevron-left"></i>',
-                    'next' => '<i class="fa fa-chevron-right"></i>',
-                ]
-            ]
-        ]);
-        
-        // set app layout to basekit layouts
+        // Only do setup for basekit if prefix is admin
         if (isset($this->request->params['prefix']) && $this->request->params['prefix'] == 'admin') {
+
+            // load helpers
+            $controller->viewBuilder()->helpers([
+                'Url', 
+                'Gourmet/KnpMenu.Menu',
+                'Html' => ['className' => 'BootstrapUI.Html'],
+                'Form' => ['className' => 'BootstrapUI.Form'],
+                'Flash' => ['className' => 'BootstrapUI.Flash'],
+                'Paginator' => [
+                    'className' => 'BootstrapUI.Paginator',
+                    'labels' => [
+                        'prev' => '<i class="fa fa-chevron-left"></i>',
+                        'next' => '<i class="fa fa-chevron-right"></i>',
+                    ]
+                ]
+            ]);
+            
+            
+            
+            // set app layout to basekit layouts
             $controller->viewBuilder()->layout('KingLoui/BaseKit.default');
-        } 
+             
 
 
-        // show/hide theme examples and settings based on config
-        if(!Configure::read('BaseKit.NavSidebar.ShowThemeExamples'))
-            Configure::delete('BaseKit.NavSidebar.MenuItems.Theme Examples');
-        if(!Configure::read('BaseKit.NavSidebar.ShowThemeSettings'))
-            Configure::delete('BaseKit.NavSidebar.MenuItems.Theme Settings');
+            // show/hide theme examples and settings based on config
+            if(!Configure::read('BaseKit.NavSidebar.ShowThemeExamples'))
+                Configure::delete('BaseKit.NavSidebar.MenuItems.Theme Examples');
+            if(!Configure::read('BaseKit.NavSidebar.ShowThemeSettings'))
+                Configure::delete('BaseKit.NavSidebar.MenuItems.Theme Settings');
 
-        // setup menu from config
-        $setMenu = function($menu, $config) use ( &$setMenu ) {
-            foreach($config as $title => $cfg) {
-                if(isset($cfg['uri'])) {
-                    // no submenu
-                    $menu->addChild($title, $cfg);
-                } else {
-                    // with submenu
-                    $menu->addChild($title, $cfg[0]);
-                    unset($cfg[0]);
-                    $setMenu($menu[$title], $cfg);
+            // setup menu from config
+            $setMenu = function($menu, $config) use ( &$setMenu ) {
+                foreach($config as $title => $cfg) {
+                    if(isset($cfg['uri'])) {
+                        // no submenu
+                        $menu->addChild($title, $cfg);
+                    } else {
+                        // with submenu
+                        $menu->addChild($title, $cfg[0]);
+                        unset($cfg[0]);
+                        $setMenu($menu[$title], $cfg);
+                    }
                 }
-            }
-        };
-        $menu = $controller->Menu->get("menu_admin");
-        $setMenu($menu, Configure::read('BaseKit.NavSidebar.MenuItems'));
+            };
+            $menu = $controller->Menu->get("menu_admin");
+            $setMenu($menu, Configure::read('BaseKit.NavSidebar.MenuItems'));
 
-        // set view vars
-        $controller->set('headerElement', Configure::read('BaseKit.NavSidebar.HeaderElement'));
-        $controller->set('headerLogo', Configure::read('BaseKit.NavSidebar.HeaderLogo'));
-        $controller->set('topLinksElement', Configure::read('BaseKit.NavTop.TopLinksElement'));
+            // set view vars
+            $controller->set('headerElement', Configure::read('BaseKit.NavSidebar.HeaderElement'));
+            $controller->set('headerLogo', Configure::read('BaseKit.NavSidebar.HeaderLogo'));
+            $controller->set('topLinksElement', Configure::read('BaseKit.NavTop.TopLinksElement'));
+        }
     }
 }
