11d13dda160e57cb4912c0a9d1e75b2361039bbd
Matthias Hollerbach <matzehollerbach@web.de>
2016-11-09 09:49:35 +0100

    added ajax component

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -7,6 +7,7 @@ use Gourmet\Aroma\Core\Configure\Engine\DbConfig;
 
 Plugin::load('Gourmet/KnpMenu');
 Plugin::load('Gourmet/Aroma');
+Plugin::load('Ajax', ['bootstrap' => true]);
 
 Configure::load('KingLoui/BaseKit.basekit');
 Configure::config('BaseKit', new DbConfig(TableRegistry::get('KingLoui/BaseKit.Configurations')));
