e56f5b760fef7d8441860ea0edac14ed28014da3
Matthias Hollerbach <matzehollerbach@web.de>
2017-01-11 11:52:10 +0100

    integrated theme plugin

---
+++ config/Migrations/20170111103958_Initial.php
@@ -0,0 +1,42 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    public function up()
+    {
+
+        $this->table('configurations')
+            ->addColumn('namespace', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('path', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('value', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+          
+    }
+
+    public function down()
+    {
+        $this->dropTable('configurations');
+    }
+}
